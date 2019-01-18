a4861aac8662140e13ce1d03ff5db04cd523b823
Kyle Imbertson <kymbert@protonmail.com>
2019-01-17 20:33:08 -0600

    nifty static web pages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
+});
+
+router.get('/how-it-works', function(req, res, next) {
+    res.render('howItWorks', { title: 'How it (will) Work' });
+});
+
+router.get('/history', function(req, res, next) {
+    res.render('history', { title: 'History' });
+});
+
+router.get('/get-involved', function(req, res, next) {
+    res.render('getInvolved', { title: 'Get Involved' });
 });
 
 module.exports = router;
