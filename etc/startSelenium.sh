fac3681c827f6475af524b43d94e8a16ef7605f7
Kyle Imbertson <kymbert@protonmail.com>
2019-07-28 20:44:25 -0500

    Issue15 dependency update (#17)
    * adds prettier formatter
    
    * update dependencies

--- etc/startSelenium.sh
+++ etc/startSelenium.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 java -jar ./etc/selenium-server-standalone-3.141.59.jar -role hub &
-java -jar ./etc/selenium-server-standalone-3.141.59.jar -role node -hub http://localhost:4444/wd/hub
+java -jar ./etc/selenium-server-standalone-3.141.59.jar -role node -hub http://localhost:4444/wd/hub &
