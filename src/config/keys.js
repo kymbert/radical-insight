72cda1e83944e5b2bcead9bdf7bc71e4fcb15448
Kyle Imbertson <kymbert@protonmail.com>
2020-06-30 11:43:20 -0500

    correcting path in app.js

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,7 +1,5 @@
 if (process.env.NODE_ENVIRONMENT === "production") {
   module.exports = require("./prod");
-} else if (process.env.NODE_ENVIRONMENT === "qa") {
-  module.exports = require("./qa");
 } else {
   module.exports = require("./dev");
 }
